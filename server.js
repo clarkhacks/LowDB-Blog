2198f0c1dc0eea6966c42c7f50d2a2b7543afff3
Clark Weckmann <clarkhacks@users.noreply.github.com>
2018-02-02 12:45:41 -0600

    + Admin

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get("/admin", function (req, res) {
+    res.sendFile(__dirname + '/public/admin.html');
+});
+
 app.get("/api/v1", function (req, res) {
     res.sendFile(__dirname + '/db.json');
 });
